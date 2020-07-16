90b5ee98c62876e033a0656a153790533bba6215
Nikk.Vsr <nikhilvaliveti@gmail.com>
2020-07-16 18:10:37 +0530

    Update timer.h

--- timer.h
+++ timer.h
@@ -7,7 +7,7 @@
 
 
 extern void timer_PWM_Init();
-extern void Fix_Right_PWM(int value);
+extern void Fix_Right_PWM(int value);  //extern void Fix_Right_PWM(int) this is also right way of declaring only, but if we give formal argument here same should be used in the .c file as well
 extern void Fix_Left_PWM(int value);
 extern void Enable_PWM();
 extern void Disable_PWM();
